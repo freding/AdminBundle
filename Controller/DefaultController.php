9247fb00fff4d5e02d247265bca055a3ab6d17ef
fredericbourbigot <fredericbourbigot@10.0.2.2>
2013-05-10 11:49:25 +0200

    first commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fredb\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    
+    
+    
+    /**
+     * @Route("/test",name="test", defaults={"_locale" = "fr"}, requirements={"_locale" = "en|fr|it"})
+     */
+    public function indexAction()
+    {
+        
+        return $this->render('FredbAdminBundle:Default:index.html.twig', array());
+    }
+}
