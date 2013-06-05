1f7f83d85dabbfb8e91a491d947850e853053b13
fredericbourbigot <fredericbourbigot@10.0.2.2>
2013-06-05 15:42:00 +0200

    commit test 10

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -8,7 +8,8 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-  
+        $em = $this->kernel->getContainer()->get('doctrine.orm.entity_manager');
+        var_dump($em);
         
         $this->assertTrue(true);
     }
