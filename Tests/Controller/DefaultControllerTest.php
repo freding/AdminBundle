0456010f76a193ef584501d90500765df06205c1
fredericbourbigot <fredericbourbigot@10.0.2.2>
2013-06-05 16:51:33 +0200

    commit test 10

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -8,9 +8,8 @@ class DefaultControllerTest extends DoctrineTest
 {
     public function testIndex()
     {
-        $oTest = $this->entityManager->getRepository("FredbAdminBundle:Test")->findOneById(1);
-        var_dump($oTest);
-        
+        //$oTest = $this->entityManager->getRepository("FredbAdminBundle:Test")->findOneById(1);
+        var_dump($this->entityManager);
         $this->assertTrue(true);
     }
 }
