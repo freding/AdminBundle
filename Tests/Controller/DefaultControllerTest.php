0d2d03a005b24b87852d1d2d3b527a391f9e5bb8
fredericbourbigot <fredericbourbigot@10.0.2.2>
2013-06-10 11:31:55 +0200

    Phpunit test

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -4,7 +4,7 @@ namespace Fredb\AdminBundle\Tests\Controller;
 
 use Fredb\AdminBundle\Tests\TestCase;
 
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends TestCase
 {
     public function testIndex()
     {
