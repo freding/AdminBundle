e3022b8aaad9dd55cb5a799431c634372502b698
fredericbourbigot <fredericbourbigot@10.0.2.2>
2013-06-13 20:44:14 +0200

    composer.json update ZF version

--- Tests/Services/Row/RowFactoryTest.php
+++ Tests/Services/Row/RowFactoryTest.php
@@ -3,12 +3,23 @@
 namespace Fredb\AdminBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Finder\Finder;
 
 class RowFactoryTest extends WebTestCase
 {
     public function testGetRowClass()
     {
 
+        
+        $finder = new Finder();
+        $finder->files()->in(__DIR__."../../../../Annotations/");
+
+        foreach ($finder as $file) {
+            \Zend_Debug::dump($file);
+
+        }
+        
+        
         $this->assertTrue(true);
     }
 }
