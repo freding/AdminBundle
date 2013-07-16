501213f2752f34e882bc76034a220a58cc181fd0
fredericbourbigot <fredericbourbigot@10.0.2.2>
2013-07-16 16:45:17 +0200

    Test

--- Tests/Services/AnnotationsServicesTest.php
+++ Tests/Services/AnnotationsServicesTest.php
@@ -16,8 +16,11 @@ class AnnotationsServicesTest extends BaseTestCase
     
     public function testGetRowClass()
     {
-        
-
+        $this->createClient();
+        $this->importDatabaseSchema();
+        $c = self::$kernel->getContainer();
+        $o =$c->get("link_service");
+        \Zend_Debug::dump($o);
         
     }
     
