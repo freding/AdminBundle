695a094c7e474821bd63a70412edba8a34409e95
fredericbourbigot <fredericbourbigot@10.0.2.2>
2013-07-17 10:22:14 +0200

    Test AdminClassService

---
+++ Tests/Services/AdminClassServiceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Fredb\AdminBundle\Tests\Services;
+
+use Fredb\AdminBundle\Tests\BaseTestCase;
+
+class AdminClassServiceTest extends BaseTestCase
+{
+    private $oContainer;
+    private $oAdminClassService;
+    
+    public function setUp() {
+        parent::setUp();
+        $this->createClient();
+        $this->importDatabaseSchema();
+        $this->oContainer           = self::$kernel->getContainer();
+        $this->oAdminClassService   = $this->oContainer->get("admin_class_service");
+    }
+    
+    
+    
+    
+    
+    public function testGetALangsAvailable()
+    {
+        $aLangs = $this->oAdminClassService->getALangsAvailable();
+        $this->assertTrue(count($aLangs)>=1);
+        
+    }
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
